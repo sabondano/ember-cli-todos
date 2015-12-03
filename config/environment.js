3f99cf16a347df1b82f87dacd0b1cd46ed559d13
Sebastian Abondano <sabondano1@gmail.com>
2015-12-03 11:06:22 -0700

    Set up CORS

--- config/environment.js
+++ config/environment.js
@@ -31,7 +31,7 @@ module.exports = function(environment) {
       'default-src': "'none'",
       'script-src': "'self' 'unsafe-inline'",
       'font-src': "'self'",
-      'connect-src': "'self'",
+      'connect-src': "'self' http://localhost:*",
       'img-src': "'self' data:",
       'style-src': "'self' 'unsafe-inline'",
       'media-src': "'self'"
